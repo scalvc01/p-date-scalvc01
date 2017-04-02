b2f79035eaa9c3124c350ee4dd7b3ed26cca9452
Sandra Calvo <scalvc01@estudiantes.unileon.es>
2017-04-02 20:15:44 +0200

    Clase date terminada

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -136,5 +136,275 @@ public class Date {
 			this._anio=anio;
 		}
 	}
+	public Date tomorrow(){
+		Date tomorrow = null;
+		int dia, mes, anio;
+		dia=this._dia;
+		mes=this._mes;
+		anio=this._anio;
+		
+		dia++;
+		if(dia>this.diasDelMes(mes)){
+			dia=1;
+			mes++;
+			if(mes>12){
+				mes=1;
+				anio++;
+			}
+		}
+		try{
+			tomorrow=new Date(dia, mes, anio);
+		}catch(Exception e){
+			System.out.println(e.getMessage());	
+		}
+		return tomorrow;
+	}
+	/* Metodo que comprueba si es dia mayor que cero y si el dia introducido es apto para ese mes*/
+	private boolean esElDia(int dia){
+		return ((dia>0) && (dia<=this.diasDelMes(this._mes)));
+	}
+	/*Metodo que retorna el dia del mes*/
+	public int diasDelMes(){
+		return this.diasDelMes(this._mes);
+	}
+	/* Metodo que devuelve el nombre del mes*/
+	public String getNombreMes(){
+		String nom = null;
+		switch (this._mes){
+		case 1:
+			nom = "Enero";
+			break;
+		case 2:
+			nom = "Febrero";
+			break;
+		case 3:
+			nom = "Marzo";
+			break;
+		case 4:
+			nom = "Abril";
+			break;
+		case 5:
+			nom = "Mayo";
+			break;
+		case 6:
+			nom = "Junio";
+			break;
+		case 7:
+			nom = "Julio";
+			break;
+		case 8:
+			nom = "Agosto";
+			break;
+		case 9:
+			nom = "Septiembre";
+			break;
+		case 10:
+			nom = "Octubre";
+			break;
+		case 11:
+			nom = "Noviembre";
+			break;
+		case 12:
+			nom = "Diciembre";
+			break;
+		}
+		return nom;
+	}
+	/*Metodo que devuelve el numero de dias del mes*/
+	private int diasDelMes(int mes){
+		int n=0;
+		switch(mes){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				n=31;
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				n=30;
+				break;
+			case 2:
+				n=28;
+				break;
+			default:
+				n = -1;
+				break;
+
+		}
+		return n;
+	}
+	/* Metodo que devuelve el nombre de la tempora del año*/
+	public String getTemporada(){
+		String nom = null;
+		switch (this._mes){
+		case 1: // next
+		case 2: // next
+		case 3: 
+			nom = "Invierno";
+			break;
+		case 4: // next
+		case 5: // next
+		case 6:
+			nom = "Primavera";
+			break;
+		case 7: // next
+		case 8: // next
+		case 9:
+			nom = "Verano";
+			break;
+		case 10: // next
+		case 11: // next
+		case 12:
+			nom = "Otoño";
+			break;
+		}
+		return nom;
+	}
+	/*Metodo que devuelve el nombre del dia*/
+	private String nameOfDay(int day) {
+		String dayName;
+		switch (day) {
+		case 1: 
+			dayName = "Monday";
+			break;
+		case 2: 
+			dayName = "Tuesday";
+			break;
+		case 3: 
+			dayName = "Wednesday";
+			break;
+		case 4: 
+			dayName = "Thursday";
+			break;
+		case 5: 
+			dayName = "Friday";
+			break;
+		case 6: 
+			dayName = "Saturday";
+			break;
+		case 7: 
+			dayName = "Sunday";
+			break;
+		default:
+			dayName = "Wrong day";
+		}
+		return dayName;
+	}
+	public int daysPast(){
+		int result;
+		result = 0;
+		
+		try{
+			Date aux = new Date(1,1,this._anio);
+		
+			for ( int i = 1; i < this._mes; i++ ) {
+				result += aux.diasDelMes();
+				aux.setMonth(i + 1);
+			}
+		} catch (DateException e){
+			System.err.println("Date.daysPast: " + e.getMessage());
+		}
+		
+		return result + this._dia - 1;
+	}
+	public boolean isSameDay(Date other){
+		if(this._dia==other.getDay()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public boolean isSameMonth(Date other){
+		if(this._mes==other.getMonth()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public boolean isSameYear (Date other){
+		return this._anio==other.getAnio();
+	}
+	public boolean isSame (Date other){
+		return this.isSameDay(other) && this.isSameMonth(other) && this.isSameYear(other);
+	}
+	public String dayOfWeek(int firstOfJanuary){
+		int dayNumber;
+		
+		dayNumber = ( daysPast() % 7 + firstOfJanuary ) % 7;
+		
+		return nameOfDay(dayNumber);
+	}
 	
+	public String toString(){
+		return this._dia + "/" + this._mes + "/" + this._anio;
+	}
+		public int numRandomTriesEqualDate(){
+        int tries, d, m, y;
+        tries = 0;
+        
+        try{
+        	do{
+        		m = (int) (Math.random()*12) + 1;
+        		d = (int) (Math.random()*this.diasDelMes(m) ) + 1;
+        		y = this._anio;
+        		tries++;
+        	} while ( !this.isSame(new Date(d,m,y) ) );
+		} catch (DateException e){
+			System.err.println("Date.numRandomTriesEqualsDate: " + e.getMessage());
+		}
+
+        return tries;
+    }
+	public String getMonthsLeft(){
+		Date aux = this;
+		StringBuffer monthsLeft = new StringBuffer();
+		
+		try{
+			for (int i = this._mes + 1; i <= 12; i++){
+				aux.setMonth(i);
+				monthsLeft.append(aux.getNombreMes() + " ");
+			}
+		} catch (DateException e){
+			System.err.println("Date.getMonthsLeft: " + e.getMessage());
+		}
+		return monthsLeft.toString();
+	}
+	
+	public String getDaysLeftOfMonth(){
+		Date aux = tomorrow();
+		StringBuffer daysLeft = new StringBuffer();
+		
+		try{
+			for (int i = aux.getDay(); esElDia(i); i++) {
+				aux.setDay(i);
+				daysLeft.append(aux.toString() + " ");
+			}
+		} catch (DateException e){
+			System.err.println("Date.getDaysLeftOfMonth: " + e.getMessage());
+		}
+		return daysLeft.toString();
+	}
+	
+	public String getMonthsSameDays(){
+		Date aux = this;
+		StringBuffer months = new StringBuffer();
+
+		try{
+			for ( int i = 1; i <= 12; i++) {
+				aux.setMonth(i);
+				if ( aux.diasDelMes() == this.diasDelMes() ) {
+					months.append(aux.getNombreMes() + " ");
+				}
+			}
+		} catch (DateException e){
+			System.err.println("Date.getMonthsSameDays: " + e.getMessage());
+		}
+		return months.toString();
+	}
 }
